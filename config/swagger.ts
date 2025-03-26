84f7f1f80f033260cef49421ce9dd972fd9d850c
eeluwole1 <engrwolesax@gmail.com>
2025-03-26 13:43:44 -0500

    feat: configure Swagger UI for API documentation at /api-docs

---
+++ config/swagger.ts
@@ -0,0 +1,27 @@
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import { Express } from "express";
+
+// define swagger options
+const swaggerOptions: swaggerJsDoc.Options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Music Event API",
+            version: "1.0.0",
+            description: 'API docs for event organizers, artists & attendees'
+        },
+    },
+    // path to annotated files
+    // **TODO** update to use routes instead of app 
+    apis: ["./src/app.ts"],
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const swaggerDocs: any = swaggerJsDoc(swaggerOptions);
+
+const setupSwagger = (app: Express): void => {
+	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+};
+
+export default setupSwagger;
